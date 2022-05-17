34e8da99bd04377d9bec59d7f63fa7ab0a66c5f8
guiroos <guilherme.roosr@gmail.com>
2022-05-17 00:47:42 -0300

    feat: created registerService

---
+++ back-end/src/services/registerService.js
@@ -0,0 +1,18 @@
+const { User } = require('../database/models');
+const { status } = require('../utils');
+
+async function register(name, email, password, role) {
+  const userCheck = await User.findOne({ where: { name, email } });
+
+  if (userCheck) {
+    return { status: status.CONFLICT, message: 'User already exists' };
+  }
+
+  const userCreated = await User.create({
+    name, email, password, role,
+  });
+
+  return { status: status.CREATED, message: userCreated };
+}
+
+module.exports = { register };
