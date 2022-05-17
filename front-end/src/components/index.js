24c5e133db343a377fa1012a6266a3592f8dbb4c
guiroos <guilherme.roosr@gmail.com>
2022-05-17 15:43:22 -0300

    faet: created CheckoutButton component

--- front-end/src/components/index.js
+++ front-end/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
 export { default as Navbar } from './Navbar';
-export { default as ProductCard } from './ProductCard.jsx';
+export { default as ProductCard } from './ProductCard';
+export { default as ProductCheckoutButton } from './ProductCheckoutButton';
