945a7ce6425de3f7861b8ecc48246faf58fd2868
guiroos <guilherme.roosr@gmail.com>
2022-05-17 16:39:35 -0300

    feat: created sellerController

--- back-end/src/controllers/index.js
+++ back-end/src/controllers/index.js
@@ -1,5 +1,8 @@
 const loginController = require('./loginController');
 const registerController = require('./registerController');
 const productController = require('./productController');
+const sellerController = require('./sellerController');
 
-module.exports = { loginController, registerController, productController };
+module.exports = {
+  loginController, registerController, productController, sellerController,
+};
