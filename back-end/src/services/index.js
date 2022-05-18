7b6418b8cedd058da727fdecbf5219fc94405c9a
guiroos <guilherme.roosr@gmail.com>
2022-05-18 15:12:15 -0300

    feat: created saleService

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -2,8 +2,9 @@ const loginService = require('./loginService');
 const registerService = require('./registerService');
 const productService = require('./productService');
 const sellerService = require('./sellerService');
-const checkoutService = require('./checkoutService');
+const customerService = require('./customerService');
+const saleService = require('./saleService');
 
 module.exports = {
-  loginService, registerService, productService, sellerService, checkoutService,
+  loginService, registerService, productService, sellerService, customerService, saleService,
 };
