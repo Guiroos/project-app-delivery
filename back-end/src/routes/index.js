2fc0f97f9fbecaa684825664080708cd9dc74272
guiroos <guilherme.roosr@gmail.com>
2022-05-18 13:45:39 -0300

    feat: create customerRoute

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -2,7 +2,8 @@ const loginRoute = require('./login');
 const registerRoute = require('./register');
 const productRoute = require('./product');
 const sellerRoute = require('./seller');
+const customerRoute = require('./customer');
 
 module.exports = {
-  loginRoute, registerRoute, productRoute, sellerRoute,
+  loginRoute, registerRoute, productRoute, sellerRoute, customerRoute,
 };
