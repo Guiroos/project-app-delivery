06e8c49e593a87981c4cfbf88d6ddea3019e1f9d
guiroos <guilherme.roosr@gmail.com>
2022-05-18 13:45:20 -0300

    feat: create checkoutService

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -2,7 +2,8 @@ const loginService = require('./loginService');
 const registerService = require('./registerService');
 const productService = require('./productService');
 const sellerService = require('./sellerService');
+const checkoutService = require('./checkoutService');
 
 module.exports = {
-  loginService, registerService, productService, sellerService,
+  loginService, registerService, productService, sellerService, checkoutService,
 };
