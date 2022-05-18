23b5b4da9c25d519da784b38f8994b2eacae7e25
guiroos <guilherme.roosr@gmail.com>
2022-05-18 13:45:28 -0300

    feat: create checkoutController

--- back-end/src/controllers/index.js
+++ back-end/src/controllers/index.js
@@ -2,7 +2,8 @@ const loginController = require('./loginController');
 const registerController = require('./registerController');
 const productController = require('./productController');
 const sellerController = require('./sellerController');
+const checkoutController = require('./checkoutController');
 
 module.exports = {
-  loginController, registerController, productController, sellerController,
+  loginController, registerController, productController, sellerController, checkoutController,
 };
