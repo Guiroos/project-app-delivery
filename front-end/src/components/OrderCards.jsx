428098385807b95f1474c8d09c97743f23fb54e9
guiroos <guilherme.roosr@gmail.com>
2022-05-23 20:45:59 -0300

    refactor: orderCards and orderDetails to both seller and customer

--- front-end/src/components/OrderCards.jsx
+++ front-end/src/components/OrderCards.jsx
@@ -3,25 +3,53 @@ import { useNavigate } from "react-router-dom";
 import { formatDate, validPrice } from "../services";
 import Card from "react-bootstrap/Card";
 
-export default function OrderCards({ order, index, statusColor }) {
+export default function OrderCards({ order, index, userRole, statusColor }) {
   const navigate = useNavigate();
 
-  return (
-    <Card
-      className="orders_card"
-      onClick={() => navigate(`/customer/orders/${order.id}`)}
-    >
-      <Card.Header>
-        <Card.Title>{`Pedido ${index + 1}`}</Card.Title>
-      </Card.Header>
-      <Card.Body style={{ backgroundColor: statusColor }}>
-        <Card.Text>{order.status}</Card.Text>
-      </Card.Body>
-      <Card.Footer>
-        <Card.Subtitle className="">{`R$ ${validPrice(
-          order.totalPrice
-        )} - ${formatDate(order.saleDate)}`}</Card.Subtitle>
-      </Card.Footer>
-    </Card>
-  );
+  const renderCards = () => {
+    if (userRole === "customer") {
+      return (
+        <Card
+          className="orders_card"
+          onClick={() => navigate(`/customer/orders/${order.id}`)}
+        >
+          <Card.Header>
+            <Card.Title>{`Pedido ${index + 1}`}</Card.Title>
+          </Card.Header>
+          <Card.Body style={{ backgroundColor: statusColor }}>
+            <Card.Text>{order.status}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Card.Subtitle className="">{`R$ ${validPrice(
+              order.totalPrice
+            )} - ${formatDate(order.saleDate)}`}</Card.Subtitle>
+          </Card.Footer>
+        </Card>
+      );
+    } else if (userRole === "seller") {
+      return (
+        <Card
+          className="orders_card"
+          onClick={() => navigate(`/seller/orders/${order.id}`)}
+        >
+          <Card.Header>
+            <Card.Title>{`Pedido ${index + 1}`}</Card.Title>
+          </Card.Header>
+          <Card.Body style={{ backgroundColor: statusColor }}>
+            <Card.Text>{order.status}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Card.Subtitle className="">{`R$ ${validPrice(
+              order.totalPrice
+            )} - ${formatDate(order.saleDate)}`}</Card.Subtitle>
+          </Card.Footer>
+          <Card.Footer>
+            <Card.Subtitle>{order.deliveryAddress}</Card.Subtitle>
+          </Card.Footer>
+        </Card>
+      );
+    }
+  };
+
+  return <>{renderCards()}</>;
 }
