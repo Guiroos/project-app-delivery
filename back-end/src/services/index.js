98432347538d0d98da6288203411392165a6919d
guiroos <guilherme.roosr@gmail.com>
2022-05-24 15:09:00 -0300

    feat: created adminService and userService

--- back-end/src/services/index.js
+++ back-end/src/services/index.js
@@ -4,7 +4,16 @@ const productService = require('./productService');
 const sellerService = require('./sellerService');
 const customerService = require('./customerService');
 const saleService = require('./saleService');
+const adminService = require('./adminService');
+const userService = require('./userService');
 
 module.exports = {
-  loginService, registerService, productService, sellerService, customerService, saleService,
+  loginService,
+  registerService,
+  productService,
+  sellerService,
+  customerService,
+  saleService,
+  adminService,
+  userService,
 };
