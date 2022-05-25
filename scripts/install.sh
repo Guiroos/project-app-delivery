2b19929ca4a0e84090bad7cd613b676869f7b38b
guiroos <guilherme.roosr@gmail.com>
2022-05-25 10:38:39 -0300

    feat: created install script for project

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+printf "\n>Preparando diretórios\n"
+
+printf "\n>Instalando backend\n"
+cd ./back-end
+npm install --prefer-dedupe
+
+printf "\n>Voltando a raiz do projeto\n"
+cd ..
+
+printf "\n>Instalando frontend\n"
+cd ./front-end
+npm install --prefer-dedupe
+
