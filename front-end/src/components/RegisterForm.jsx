bff82f8cb4873046d11fd5ce70cd691961026160
guiroos <guilherme.roosr@gmail.com>
2022-05-31 20:58:52 -0300

    style: Register page and components

--- front-end/src/components/RegisterForm.jsx
+++ front-end/src/components/RegisterForm.jsx
@@ -7,8 +7,6 @@ import {
   apiPostBody,
   saveToLocalStorage,
 } from "../services";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 
 export default function RegisterForm() {
   const [errorMessage, setErrorMessage] = useState("");
@@ -43,70 +41,93 @@ export default function RegisterForm() {
   };
 
   return (
-    <Form className="register_form" onSubmit={handleSubmit(onSubmit)}>
-      <Form.Group controlId="name">
-        <Form.Label>Nome</Form.Label>
-        <Form.Control
-          type="name"
-          name="name"
-          placeholder="Seu nome"
-          {...register("name", {
-            required: true,
-            minLength: {
-              value: 12,
-              message: "Name must be at least 12 characters",
-            },
-          })}
-        />
-      </Form.Group>
+    <div className="w-full min-w-sm max-w-lg md:text-xl lg:text-2xl">
+      <form
+        className="bg-slate-100 shadow-md rounded px-8 py-8 mb-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="mb-5">
+          <label className="block mb-3" htmlFor="name">
+            Nome
+          </label>
+          <input
+            type="name"
+            id="name"
+            placeholder="Seu nome"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            {...register("name", {
+              required: true,
+              minLength: {
+                value: 1,
+                message: "Name must have at least one character",
+              },
+            })}
+          />
+          {errors.name && (
+            <p className="mt-1 text-red-500 text-sm">{errors.name.message}</p>
+          )}
+        </div>
 
-      <Form.Group controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          placeholder="seu-email@site.com.br"
-          {...register("email", {
-            required: true,
-            pattern: {
-              value: EMAIL_PATTERN,
-              message: "Invalid email",
-            },
-          })}
-        />
-      </Form.Group>
+        <div className="mb-5">
+          <label>Email</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="seu-email@site.com.br"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            {...register("email", {
+              required: true,
+              pattern: {
+                value: EMAIL_PATTERN,
+                message: "Invalid email",
+              },
+            })}
+          />
+          {errors.email && (
+            <p className="mt-1 text-red-500 text-sm">{errors.email.message}</p>
+          )}
+        </div>
 
-      <Form.Group controlId="password">
-        <Form.Label>Senha</Form.Label>
-        <Form.Control
-          type="password"
-          name="password"
-          placeholder="******"
-          {...register("password", {
-            required: true,
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters",
-            },
-          })}
-        />
-      </Form.Group>
+        <div className="mb-5">
+          <label>Senha</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="******"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+            })}
+          />
+          {errors.password && (
+            <p className="mt-1 text-red-500 text-sm">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
 
-      <Form.Group controlId="buttons">
-        <Button variant="primary" onClick={() => navigate("/login")}>
-          Voltar ↩️
-        </Button>
-        <Button type="submit" disabled={!isValid}>
-          Cadastrar
-        </Button>
-      </Form.Group>
-
-      <Form.Group controlId="errors">
-        {errors.name && <p>{errors.name.message}</p>}
-        {errors.email && <p>{errors.email.message}</p>}
-        {errors.password && <p>{errors.password.message}</p>}
+        <div className="flex items-center justify-around text-white">
+          <button
+            className="bg-purple-500 hover:bg-purple-700 py-3 px-4 rounded-md font-bold cursor-pointer"
+            type="button"
+            onClick={() => navigate("/login")}
+          >
+            Go back
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 py-3 px-4 rounded-md font-bold cursor-pointer"
+            type="submit"
+            disabled={!isValid}
+          >
+            Register
+          </button>
+        </div>
         {errorMessage && <p>{errorMessage}</p>}
-      </Form.Group>
-    </Form>
+      </form>
+    </div>
   );
 }
