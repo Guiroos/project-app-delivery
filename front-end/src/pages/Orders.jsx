3df7cca94fb10d1f0ed122e2022ea42ec5adabbe
guiroos <guilherme.roosr@gmail.com>
2022-06-02 10:46:47 -0300

    style: Orders component and page

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, OrderCards } from "../components";
 import { apiGet, changeStatusColor, getItemLocalStorage } from "../services";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 import logo from "../images/doughnut_logo.png";
 
@@ -25,7 +22,7 @@ export default function Orders() {
         } catch (err) {
           console.log(err);
         }
-      } else if(userRole === "seller") {
+      } else if (userRole === "seller") {
         try {
           setIsLoading(true);
           const { email } = getItemLocalStorage("user");
@@ -49,22 +46,18 @@ export default function Orders() {
       );
     } else {
       return (
-        <main className="orders_page animate-bottom">
+        <main className="animate-bottom">
           <Navbar />
-          <Container className="orders_page_container" fluid="md">
-            <Row xs={2} md={4} className="g-4 orders_page_container_cards">
-              {orderDetails.map((order, index) => (
-                <Col key={index}>
-                  <OrderCards
-                    order={order}
-                    index={index}
-                    userRole={userRole}
-                    statusColor={changeStatusColor(order.status)}
-                  />
-                </Col>
-              ))}
-            </Row>
-          </Container>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 p-16">
+            {orderDetails.map((order, index) => (
+              <OrderCards
+                order={order}
+                index={index}
+                userRole={userRole}
+                statusColor={changeStatusColor(order.status)}
+              />
+            ))}
+          </div>
         </main>
       );
     }
