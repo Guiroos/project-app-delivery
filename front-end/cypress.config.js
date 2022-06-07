1296c14b1dd2cc7d010354bd6d9f899bf3b63b39
guiroos <guilherme.roosr@gmail.com>
2022-06-07 17:10:12 -0300

    feat: add projectId and removed nodeEvents in cypress config

--- front-end/cypress.config.js
+++ front-end/cypress.config.js
@@ -1,10 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'xfmvkh',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: "http://localhost:3000",
   },
 });
